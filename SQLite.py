209483859a1d0f51f5f0b003148a68c01f2966d0
Jimao <534349953@qq.com>
2018-08-03 15:59:08 +0800

    hello,123

--- SQLite.py
+++ SQLite.py
@@ -3,3 +3,12 @@ import sqlite3
 
 #连接到SQLite数据库
 #数据库文件是test.db
+#如果文件不存在，会自动在当前目录创建
+conn = sqlite3.connect('test.db')
+
+#创建一个Cursor
+cursor = conn.cursor()
+
+cursor.execute('create table user (id number (20) primary key,name varchar(20))')
+
+
